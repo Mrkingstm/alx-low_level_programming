e16258943278b017fd10f4806bd8961bf52ff6c7
Mrkingstm <kingsley1.kn@gmail.com>
2022-09-08 08:14:00 -0700

    TASK #

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
  * main - print the srting in the function
  *
  * Description: using the main function
- * this program prints "programing is like building a multilingual puzzle"
+ * this program prints "programing is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("programming is like building a multilingual puzzle");
 	return (0);
 }
