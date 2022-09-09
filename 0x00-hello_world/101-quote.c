13e2e191d24648b0d59415763178d5837ccdfede
Mrkingstm <kingsley1.kn@gmail.com>
2022-09-09 16:19:41 -0700

    TASK#

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+#include<stdib.h>
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	system("rm barhamounafissa > sorti 2>&1");
+	exit(1);
+}
