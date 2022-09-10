ed2d6523540709eb0165ae3d469ee9a431604f64
Mrkingstm <kingsley1.kn@gmail.com>
2022-09-09 17:07:29 -0700

    TASK#

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - Betty
- *
- * Return: 0 after print the function
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success
  */
 int main(void)
 {
